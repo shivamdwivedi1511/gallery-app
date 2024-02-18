d6d0e1c7dce4dcbbf02fc88dbc18d35cc4df04f8
shivamdwivedi1511 <shivamdwivedi1511@gmail.com>
2024-02-18 23:59:22 +0530

    use context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { createContext } from "react";
 import "./App.scss";
 import Gallery from "./components/Gallery";
 
 function App() {
+  const userContext = createContext({ isAutheticated: false });
   return (
     <div className="app">
       <h1 className="heading">gallery app</h1>
-      <Gallery />
+      <userContext.Provider value={{ isAutheticated: true }}>
+        <Gallery />
+      </userContext.Provider>
     </div>
   );
 }
